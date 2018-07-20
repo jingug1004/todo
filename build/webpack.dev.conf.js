f1359c303314f387b7fd5bc4e333f2df51bbdc43
jingug1004 <kjg4989q>
2018-07-20 22:05:47 +0900

    180720(금) 오늘 한 일 - 내일 할 일 - 도움이 필요한 일 - 예상되는 오류, 에러, 이슈 -
    긴급 || 중요
    
    긴급 || 안 중요
    
    안 긴급 || 중요
    
    안 긴급 || 안 중요

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -9,13 +9,14 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
-
+const env = require('../config/dev.env') // 추가 1/3
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
+console.log("lll~~~ env : " + env);
 
 const devWebpackConfig = merge(baseWebpackConfig, {
   module: {
-    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
+    rules: utils.styleLoaders({sourceMap: config.dev.cssSourceMap, usePostCSS: true})
   },
   // cheap-module-eval-source-map is faster for development
   devtool: config.dev.devtool,
@@ -25,7 +26,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     clientLogLevel: 'warning',
     historyApiFallback: {
       rewrites: [
-        { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
+        {from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html')},
       ],
     },
     hot: true,
@@ -35,17 +36,19 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     port: PORT || config.dev.port,
     open: config.dev.autoOpenBrowser,
     overlay: config.dev.errorOverlay
-      ? { warnings: false, errors: true }
+      ? {warnings: false, errors: true}
       : false,
     publicPath: config.dev.assetsPublicPath,
     proxy: config.dev.proxyTable,
     quiet: true, // necessary for FriendlyErrorsPlugin
     watchOptions: {
       poll: config.dev.poll,
-    }
+    },
+    disableHostCheck: true // 추가 2/3
   },
   plugins: [
     new webpack.DefinePlugin({
+      'TEST': "TEST", // 추가 3/3
       'process.env': require('../config/dev.env')
     }),
     new webpack.HotModuleReplacementPlugin(),
@@ -85,8 +88,8 @@ module.exports = new Promise((resolve, reject) => {
           messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
         },
         onErrors: config.dev.notifyOnErrors
-        ? utils.createNotifierCallback()
-        : undefined
+          ? utils.createNotifierCallback()
+          : undefined
       }))
 
       resolve(devWebpackConfig)
